85c45ef431ade3a12ae2bd9e6bcb3fcf710a3428
Collin Yen <collinyen7@gmail.com>
2014-05-01 22:28:26 -0700

    Initial commiting iOS project files

---
+++ ScheduleSharer/ScheduleSharer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ScheduleSharer
+//
+//  Created by Collin Yen on 5/1/14.
+//  Copyright (c) 2014 Collin Yen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ScheduleSharer/ScheduleSharer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ScheduleSharer
+//
+//  Created by Collin Yen on 5/1/14.
+//  Copyright (c) 2014 Collin Yen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
